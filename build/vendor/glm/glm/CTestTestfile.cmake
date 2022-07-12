d87abe679534d1adaf6140c910aa89b73e1970e7
Tarek <tarekspeed7@gmail.com>
2022-07-12 19:10:11 +0200

    Delete cmake build files

--- build/vendor/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tarek/raytracer/vendor/glm/glm
-# Build directory: /home/tarek/raytracer/build/vendor/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
