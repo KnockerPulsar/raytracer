e3766c68407fff392fcf0a30dc57b05769c7482d
Tarek Yasser <tarekspeed7@gmail.com>
2023-04-09 03:16:01 +0200

    Add final scene in book 2.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "constant_medium.h"
 #include "hittable.h"
 #include "moving_sphere.h"
+#include "random.h"
 #include "raytracer.h"
 #include "hittable_list.h"
 #include "scene.h"
@@ -271,6 +272,87 @@ void cornell_smoke(scene& scene_desc) {
 
 		scene_desc.world = hittable_list(make_shared<bvh_node>(world));
 }
+
+void final_scene(scene& scene_desc) {
+    scene_desc.windowWidth = 800;
+    scene_desc.windowHeight = 800;
+    scene_desc.samplesPerPixel = 10000;
+    scene_desc.renderScale = 0.1;
+    scene_desc.background        = color(0,0,0);
+
+    scene_desc.cam.aperture = 0.0;
+    scene_desc.cam.vFov     = 40.0;
+    scene_desc.cam.lookfrom = point3(478, 278, -600);
+    scene_desc.cam.lookat   = point3(278, 278, 0);
+
+    hittable_list boxes1;
+    auto ground = make_shared<lambertian>(color(0.48, 0.83, 0.53));
+
+		const int boxes_per_side = 20;
+		for (int i = 0; i < boxes_per_side; i++) {
+			for (int j = 0; j < boxes_per_side; j++) {
+				auto w = 100;
+				auto x0 = -1000 + i * w;
+				auto z0 = -1000 + j * w;
+				auto y0 = 0.0;
+
+				auto x1 = x0 + w;
+				auto y1 = randomFloat(1, 101);
+				auto z1 = z0 + w;
+
+				boxes1.add(box(point3(x0, y0, z0), point3(x1, y1, z1), ground));
+			}
+		}
+
+		hittable_list& world = scene_desc.world;
+
+		world.add(make_shared<bvh_node>(boxes1));
+	
+		auto light = make_shared<diffuse_light>(color(7, 7, 7));
+		world.add(make_shared<quad>(point3(123, 554, 147), vec3(300, 0, 0), vec3(0, 0, 265), light));
+
+		auto center1 = point3(400, 400, 200);
+		auto center2 = center1 + vec3(30, 0, 0);
+		auto moving_sphere_material = make_shared<lambertian>(color(0.7, 0.3, 0.1));
+		world.add(make_shared<moving_sphere>(center1, center2, 50, moving_sphere_material));
+
+		world.add(make_shared<sphere>(point3(260, 150, 45), 50, make_shared<dielectric>(1.5)));
+		world.add(make_shared<sphere>(point3(0, 150, 145), 50, make_shared<metal>(color(0.8, 0.8, 0.8), 1.0)));
+
+		auto boundary = make_shared<sphere>(point3(360, 150, 145), 70, make_shared<dielectric>(1.5));
+		world.add(boundary);
+		world.add(make_shared<constant_medium>(boundary, 0.2, color(0.2, 0.4, 0.9)));
+		boundary = make_shared<sphere>(point3(0, 0, 0), 5000, make_shared<dielectric>(1.5));
+		world.add(make_shared<constant_medium>(boundary, 0.0001, color(1, 1, 1)));
+
+		auto emat = make_shared<lambertian>(make_shared<image_texture>("earthmap.png"));
+		world.add(make_shared<sphere>(point3(400, 200, 400), 100, emat));
+
+		auto pertext = make_shared<noise_texture>(0.1);
+		world.add(make_shared<sphere>(point3(220, 280, 300), 80, make_shared<lambertian>(pertext)));
+
+		hittable_list boxes2;
+		auto white = make_shared<lambertian>(color(.73, .73, .73));
+		int ns = 1000;
+		for (int j = 0; j < ns; j++) {
+			boxes2.add(make_shared<sphere>(point3::random(0, 165), 10, white));
+		}
+
+		world.add(make_shared<translate>( 
+					make_shared<rotate_y>( make_shared<bvh_node>(boxes2), 15), 
+					vec3(-100, 270, 395)
+		));
+}
+
+void default_scene(scene& scene_desc) {
+	final_scene(scene_desc);
+	scene_desc.windowWidth = 400;
+	scene_desc.windowHeight = 400;
+	scene_desc.samplesPerPixel = 200;
+	scene_desc.renderScale = 0.75;
+	scene_desc.maxDepth = 4;
+}
+
 int main() {
 	scene sceneDesc;
 	sceneDesc.background = color(.7, .8, 1.);
@@ -286,8 +368,9 @@ int main() {
 		case 5: quads(sceneDesc);              break;
 		case 6: simple_light(sceneDesc); 			 break;
 		case 7: cornell_box(sceneDesc); 			 break;
-		default:
-		case 8: cornell_smoke(sceneDesc); break;
+		case 8: cornell_smoke(sceneDesc);      break;
+		case 9: final_scene(sceneDesc);        break;
+		default: default_scene(sceneDesc);     break;
 	}
  
 	InitWindow(sceneDesc.windowWidth, sceneDesc.windowHeight, "Rayborn");
