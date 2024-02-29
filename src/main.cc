3268285588b5493e9c3ff364f4e14f658044225d
Tarek Yasser <tarekspeed7@gmail.com>
2024-02-29 23:55:37 +0200

    Chapter 11 done.

--- src/main.cc
+++ src/main.cc
@@ -10,19 +10,20 @@ int main() {
     hittable_list world;
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left   = make_shared<dielectric>(1.5);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
     world.add(std::make_shared<sphere>(point3(0.0, -100.5, -1.0), 100, material_ground));
     world.add(std::make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
     world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
     world.add(std::make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     camera cam;
 
     cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_width       = 1920;
+    cam.image_width       = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth         = 50;
 
--- src/material.h
+++ src/material.h
@@ -37,7 +37,7 @@ class lambertian : public material {
 
 class metal : public material {
   public:
-    metal(const color& a, double f) : albedo(a), fuzz(f < 1? f: 1) {}
+    metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
     bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
         const override {
@@ -51,4 +51,44 @@ class metal : public material {
   private:
     color  albedo;
     double fuzz;
+};
+
+class dielectric : public material {
+  public:
+    dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+        const override {
+        attenuation = color(1.0, 1.0, 1.0);
+
+        // 1.0 is the refraction index for air
+        // Front face means going into the object, eta_i is 1.0 (air), eta_t is the object's ir.
+        // Back face is the opposite, (eta_t / 1.0) = (ir / 1.0) = ir
+        double refraction_ratio = rec.front_face ? (1.0 / ir) : ir;
+
+        vec3   unit_direction = unit_vector(r_in.direction());
+        double cos_theta      = fmin(dot(-unit_direction, rec.normal), 1.0);
+        double sin_theta      = sqrt(1 - cos_theta * cos_theta);
+
+        bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+        vec3 direction;
+
+        if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double())
+            direction = reflect(unit_direction, rec.normal);
+        else
+            direction = refract(unit_direction, rec.normal, refraction_ratio);
+
+        scattered = ray(rec.p, direction);
+        return true;
+    }
+
+  private:
+    double ir;
+
+    static double reflectance(double cosine, double ref_idx) {
+        // Shlick's approximation
+        auto r0 = (1 - ref_idx) / (1 + ref_idx);
+        r0      = r0 * r0;
+        return r0 + (1 - r0) * pow((1 - cosine), 5);
+    }
 };
